455565f441b26567a46efe24941882593e1b5ecb
ShubhamMaske <shubhammaske099@gmail.com>
2023-03-04 13:21:29 +0530

    Fifth Commit(get,fetch, update,delete)sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,25 +2,27 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows, fieldData]) => {
-    res.render('shop/product-list', {
-      prods: rows,
-      pageTitle: 'All Products',
-      path: '/products'
+  Product.findAll()
+    .then((products) => {
+      res.render('shop/product-list', {
+        prods: products,
+        pageTitle: 'All Products',
+        path: '/products'
+      });
+    })
+    .catch(err => {
+      console.log(err);
     });
-  })
-  .catch(err => console.log(err));
   
 };
 
 exports.getProduct = (req,res,next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
+  Product.findByPk(prodId)
+    .then((product) => {
       console.log(product);
       res.render('shop/product-detail',{
-        product: product[0],
+        product: product,
         pageTitle: product.title,
         path:'/products'
       });
@@ -30,15 +32,18 @@ exports.getProduct = (req,res,next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows, fieldData]) => {
-    res.render('shop/index', {
-      prods: rows,
-      pageTitle: 'Shop',
-      path: '/'
+  Product.findAll()
+    .then((products) => {
+      res.render('shop/index', {
+        prods:products,
+        pageTitle: 'Shop',
+        path: '/'
+      });
+    })
+    .catch(err => {
+      console.log(err);
     });
-  })
-  .catch(err => console.log(err));
+
 };
 
 exports.getCart = (req, res, next) => {
@@ -50,7 +55,7 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) => {
+  Product.findByPk(prodId, (product) => {
     Cart.addProduct(prodId,product.price);
   });
   res.redirect('/cart');
