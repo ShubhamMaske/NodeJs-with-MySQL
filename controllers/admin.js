4abb3d2ce786361fe45b6ccad1a7166da746ab76
ShubhamMaske <shubhammaske099@gmail.com>
2023-03-08 10:10:56 +0530

    user and product relationship commit

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title:title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description
-  })
+  req.user
+    .createProduct({
+      title:title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description
+    })
     .then(result => {
       console.log(result);
       res.redirect('/admin/products');
@@ -33,8 +34,11 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-  .then((product) =>{
+  req.user
+    .getProducts({where: {id:prodId}})
+  // Product.findByPk(prodId)
+  .then((products) =>{
+    const product = products[0];
     if(!product){
       return res.redirect('/');
     }
